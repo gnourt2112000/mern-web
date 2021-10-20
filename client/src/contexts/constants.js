61d8129ca9e27d5edd3cf9abaec210609beea0ac
Nguyen Manh Truong <gnourt2112000@gmail.com>
2021-10-20 21:31:56 +0700

    prepare

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,3 +1,3 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' :'url'
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' :'https://vast-scrubland-90147.herokuapp.com/api'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'mern-web'
